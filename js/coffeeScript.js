353dc3fab71b5c5ee1a3f7e730a27f8bf5065065
Dwaynebok <dwaynebok19@gmail.com>
2020-12-21 13:59:57 +0100

    adding

--- js/coffeeScript.js
+++ js/coffeeScript.js
@@ -13,6 +13,15 @@ $(document).ready(function() {
 
 });
 
-<!--buttons-->
+<!--array of country's-->
 
-document.getElementsByClassName('button')
+let  countryList = [ 'Germany', 'Switzerland','Netherlands','France','Belgium','London','USA','India','Finland','Denmark','Canada','Australia','Argentina','Italy','Pakistan','Ghana','Nigeria','Spain','Mexico','Albania','Afghanistan', 'Turkey','Morocco','Ireland','Iceland'];
+
+for (let i=0;i<countryList.length;i++){
+    console.log(countryList[i]);
+}
+/*
+countryList.forEach(function(item, index, array) {
+    console.log(item, index)
+})
+ */
